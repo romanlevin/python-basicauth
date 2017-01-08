478bdb3bed5de2b9d3e3cf710ce036992ded7c24
Roman Levin <rlevin@egnyte.com>
2017-01-08 14:18:03 +0100

    Fix formatting in `setup.py`

--- setup.py
+++ setup.py
@@ -4,27 +4,24 @@ from setuptools import setup
 
 
 setup(
-
     # Basic package information:
-    name = 'basicauth',
-    version = '0.3',
-    py_modules = ('basicauth',),
+    name='basicauth',
+    version='0.3',
+    py_modules=('basicauth',),
 
     # Packaging options:
-    zip_safe = False,
-    include_package_data = True,
+    zip_safe=False,
+    include_package_data=True,
 
     # Package dependencies:
-    install_requires = [],
+    install_requires=[],
 
     # Metadata for PyPI:
-    author = 'Randall Degges',
-    author_email = 'rdegges@gmail.com',
-    license = 'UNLICENSE',
-    url = 'https://github.com/rdegges/python-basicauth',
-    keywords = 'python security basicauth http',
-    description = 'An incredibly simple HTTP basic auth implementation.',
-    long_description = open(normpath(join(dirname(abspath(__file__)),
-        'README.md'))).read()
-
+    author='Randall Degges',
+    author_email='rdegges@gmail.com',
+    license='UNLICENSE',
+    url='https://github.com/rdegges/python-basicauth',
+    keywords='python security basicauth http',
+    description='An incredibly simple HTTP basic auth implementation.',
+    long_description=open(normpath(join(dirname(abspath(__file__)), 'README.md'))).read()
 )
